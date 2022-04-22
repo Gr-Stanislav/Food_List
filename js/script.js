22d6b8e72470d8e3a7000385b7f0be14afc5fe73
Gr-Stanislav <grstas1990@gmail.com>
2022-04-22 23:43:38 +0300

    reg timer

--- js/script.js
+++ js/script.js
@@ -48,16 +48,26 @@ window.addEventListener('DOMContentLoaded', function() {
 
     // Timer
 
-    const deadLine = '2022-10-15';
+    const deadLine = '2022-12-31';
 
     function getTimeRemaining (endtime){
-        const t = Date.parse(endtime) - Date.parse(new Date ()),
-              days = Math.floor(t / (1000 * 60 * 60 * 24)),
-              hours = Math.floor((t / (1000 * 60 * 60) % 24)),
-              minutes = Math.floor((t / 1000 / 60) % 60),
-              seconds = Math.floor((t / 1000) % 60);
+        let days, hours, minutes, seconds;
 
-              return {
+        const t = Date.parse(endtime) - Date.parse(new Date ());
+
+        if (t <= 0) {
+            days = 0;
+            hours = 0;
+            minutes = 0;
+            seconds = 0;
+        } else {
+            days = Math.floor(t / (1000 * 60 * 60 * 24)),
+            hours = Math.floor((t / (1000 * 60 * 60) % 24)),
+            minutes = Math.floor((t / 1000 / 60) % 60),
+            seconds = Math.floor((t / 1000) % 60);
+        }
+
+         return {
                 'total': t,
                 'days': days,
                 'hours': hours,
