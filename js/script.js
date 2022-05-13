9c7b8fa42b5d97c0d46e40d7c61bf46be444663b
Gr-Stanislav <grstas1990@gmail.com>
2022-05-13 13:56:46 +0300

    add fetch

--- js/script.js
+++ js/script.js
@@ -217,7 +217,7 @@ window.addEventListener('DOMContentLoaded', function() {
 
     const forms = document.querySelectorAll('form');
     const message = {
-        loading: 'Загрузка...',
+        loading: 'img/Spinner.svg',
         success: 'Спасибо! Скоро мы с вами свяжемся',
         failure: 'Что-то пошло не так...'
     };
@@ -236,32 +236,36 @@ window.addEventListener('DOMContentLoaded', function() {
             statusMessage.textContent = message.loading;
             form.appendChild(statusMessage);
         
-            const request = new XMLHttpRequest();
-            request.open('POST', 'server.php');
-            request.setRequestHeader('Content-type', 'application/json; charset=utf-8');
             const formData = new FormData(form);
 
             const object = {};
             formData.forEach(function(value, key){
                 object[key] = value;
             });
-            const json = JSON.stringify(object);
 
-            request.send(json);
-
-            request.addEventListener('load', () => {
-                if (request.status === 200) {
-                    console.log(request.response);
-                    statusMessage.textContent = message.success;
-                    form.reset();
-                    setTimeout(() => {
-                        statusMessage.remove();
-                    }, 2000);
-                } else {
-                    statusMessage.textContent = message.failure;
-                }
-            });
+            fetch('server.php', {
+                method: "POST",
+                headers: {
+                    'Content-type': 'application/json'
+                },
+                body: JSON.stringify(object)
+            })
+            .then(data => data.text())
+            .then(data => {
+                console.log(data);
+                statusMessage.textContent = message.success;
+                setTimeout(() => {
+                    statusMessage.remove();
+                }, 2000);
+            }).catch(() => {
+                statusMessage.textContent = message.failure;
+                setTimeout(() => {
+                    statusMessage.remove();
+                }, 2000);
+            }).finally(() => {
+                form.reset();
+            })
         });
-    }
+        
 
 });
